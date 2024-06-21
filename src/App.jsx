84b9a0183017110c7e9fee6b314115e1dbf506e8
jeet1703 <prabhjeetdec03@gmail.com>
2024-06-21 06:02:26 +0530

    day 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 
 
 import Hero from './Components/Hero';
-import Brand from './Components/Brand';
 import ContactUs from './Components/Contactus';
 import Footer from './Components/Footer';
+import Accordion from './Components/Accordion';
+import Testimonial from './Components/Testimonials';
+import Company from './Components/Company';
+import DividerSvg from './Components/Divider';
 
 function App() {
 
   return (
     <>
       <Hero/>
-      {/* <Brand/> */}
+      <Company/>
+      <Testimonial/>
+      <DividerSvg/>
+      <Accordion/>
+      <DividerSvg/>
       <ContactUs/>
       <Footer/>
     </>
