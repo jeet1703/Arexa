68123570823ab36985d73b9cf626a1e60d6e8fbb
jeet1703 <prabhjeetdec03@gmail.com>
2024-06-24 16:47:18 +0530

    Image Changes

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
     >
       <div className="absolute inset-0 from-[#140639] via-[#7961BA] to-[#15073A] opacity-90 z-0"></div>
       <Header />
-      <div className="container relative mx-auto h-full flex flex-col lg:flex-row items-center z-10">
+      <div className="relative mx-auto h-full flex flex-col lg:flex-row items-center z-10">
         <div className="w-full lg:w-1/2 p-8 text-center lg:text-left">
           <h1 className="text-3xl md:text-4xl lg:text-6xl text-white mt-20 lg:mt-0">
             Design Revolution Through AR Innovation
@@ -50,11 +50,11 @@ const Hero = () => {
             </a>
           </div>
         </div>
-        <div className="relative w-full lg:w-5/12 flex justify-center lg:justify-end lg:absolute lg:bottom-0 lg:right-0">
+        <div className="relative w-full lg:w-5/12 flex justify-center md:justify-end md:absolute md:bottom-0 md:right-0">
           <img
             src={Person}
             alt="Description of image"
-            className="relative w-full max-w-xs sm:max-w-sm md:max-w-sm lg:max-w-full h-auto sm:mb-0 md:mb-4 lg:mb-0"
+            className="relative w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg h-auto sm:absolute sm:bottom-0 md:static md:bottom-0"
           />
         </div>
       </div>
